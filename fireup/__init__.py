bdf25de60573e918b051dcbff81dfb680b26b6a0
lee <emctopgear@gmail.com>
2019-07-20 15:08:15 +0800

    more algorithms are impending

--- fireup/__init__.py
+++ fireup/__init__.py
@@ -10,6 +10,10 @@ from fireup.algos.dqn.dqn import dqn
 # more algorithms
 from fireup.algos.double_dqn.double_dqn import double_dqn
 from fireup.algos.dueling_dqn.dueling_dqn import dueling_dqn
+from fireup.algos.noisy_dqn.noisy_dqn import noisy_dqn
+from fireup.algos.per_dqn.per_dqn import per_dqn
+from fireup.algos.deepq.deepq import deepq
+
 from fireup.algos.c51.c51 import c51
 from fireup.algos.qr_dqn.qr_dqn import qr_dqn
 
